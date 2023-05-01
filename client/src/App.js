d0fa5ee63ce6edae3db9c3ba3333e404b20202b6
BenLopez00 <lop.ben00@gmail.com>
2023-05-01 20:53:10 -0300

    Agregados paises (detalles + imagenes) + actividades y paginado

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import {BrowserRouter, Route, Switch} from "react-router-dom"
 import Home from './Components/Home/Home';
+import CreateActivity from './Components/CreateActivity/CreateActivity';
+import CountryDetail from './Components/CountryDetail/CountryDetail';
+
 
 function App() {
   return (
@@ -9,7 +12,10 @@ function App() {
       <Switch>
         <Route exact path= "/" component = {Home}/>
         <Route path= "/home" component = {Home}/>
+        <Route path= "/activities" component= {CreateActivity}/>
+        <Route path= "/countries/:id" component= {CountryDetail}/>
       </Switch>
+      
     </div>
     </BrowserRouter>
   );
